9ee87c194d228866be9b5ce1a60f49b01b3ace6e
Yevhenii Lukashenko <freewindsua@gmail.com>
2018-10-26 10:34:21 +0300

    Add Ukrainian lang to dojo nls

---
+++ nls/ua/common.js
@@ -0,0 +1,96 @@
+// COPYRIGHT © 2018 Esri
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// This material is licensed for use under the Esri Master License
+// Agreement (MLA), and is bound by the terms of that agreement.
+// You may redistribute and use this code without modification,
+// provided you adhere to the terms of the MLA and include this
+// copyright notice.
+//
+// See use restrictions at http://www.esri.com/legal/pdfs/mla_e204_e300/english
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, USA 92373
+// USA
+//
+// email: contracts@esri.com
+//
+// See http://js.arcgis.com/4.9/esri/copyright.txt for details.
+
+define({
+    about: "Інформація про",
+    add: "Додати",
+    all: "все",
+    apply: "Застосувати",
+    auth: {logOut: "Вихід", signIn: "Увійти"},
+    back: "Назад",
+    cancel: "Скасувати",
+    change: "Змінити",
+    clear: "Очистити",
+    close: "Закрити",
+    collapse: "Згорнути",
+    configure: "Налаштування",
+    control: {pause: "Призупинити", play: "Відтворення", resume: "Відновити", stop: "Стоп"},
+    copy: "Копіювати",
+    create: "Створити",
+    cut: "Вирізати",
+    delete: "Видалити",
+    details: "Подробиці",
+    edit: "Редагувати",
+    error: "Помилка",
+    esri: "Esri",
+    exit: "Вийти",
+    expand: "Розгорнути",
+    fieldsSummary: "Список атрибутів та значень",
+    find: "Пошук",
+    form: {no: "Ні", ok: "OK", password: "Пароль", submit: "Відправити", username: "Ім'я користувача", yes: "Так"},
+    help: "Довідка",
+    home: "На головну",
+    info: "Інформація",
+    information: "Інформація",
+    layer: "Шар",
+    loading: "Завантаження",
+    maximize: "Розгорнути",
+    menu: "Меню",
+    more: "Ще",
+    none: "Ні",
+    open: "Відккрити",
+    pagination: {
+        first: "Перший",
+        last: "Останній",
+        next: "Далі",
+        page: "Сторінка",
+        pageText: "{index} з {total}",
+        previous: "Повернутись"
+    },
+    paste: "Вставити",
+    percent: "{percent}%",
+    preview: "Перегляд",
+    print: "Друк",
+    publish: "Публикація",
+    redo: "Повторити",
+    refresh: "Оновити",
+    remove: "Видалити",
+    rename: "Перейменувати",
+    reset: "Скинути",
+    restore: "Відновити",
+    save: "Зберегти",
+    search: "Пошук",
+    searching: "Пошук",
+    select: "Обрати",
+    settings: "Установки",
+    sort: "Сортувати",
+    share: "Опублікувати",
+    title: "Назва",
+    untitled: "Без ім'я",
+    upload: "Завантажити",
+    undo: "Скасувати",
+    view: "Перегляд",
+    visibility: {hide: "Сховати", show: "Показувати", toggle: "Переключити"},
+    warning: "Попередження"
+});
