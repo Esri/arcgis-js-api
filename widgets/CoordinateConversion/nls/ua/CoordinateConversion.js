e73b5ef38f67dee16d97741b58c5a13346502425
Yevhenii Lukashenko <freewindsua@gmail.com>
2018-10-26 10:44:05 +0300

    Add Ukrainian lang to CoordinateConversion widget

---
+++ widgets/CoordinateConversion/nls/ua/CoordinateConversion.js
@@ -0,0 +1,25 @@
+// COPYRIGHT © 2018 Esri
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// This material is licensed for use under the Esri Master License
+// Agreement (MLA), and is bound by the terms of that agreement.
+// You may redistribute and use this code without modification,
+// provided you adhere to the terms of the MLA and include this
+// copyright notice.
+//
+// See use restrictions at http://www.esri.com/legal/pdfs/mla_e204_e300/english
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, USA 92373
+// USA
+//
+// email: contracts@esri.com
+//
+// See http://js.arcgis.com/4.9/esri/copyright.txt for details.
+
+define({abbreviatedDirections: {north: "С", south: "Ю", east: "В", west: "З"},addConversion: "Додати конвертацію",captureMode: "Режим захоплення",changeCoordinateDisplay: "Змінити відображення координат",convert: "Конвертувати",conversionOutputSuffix: "Результат конвертації",copySuccessMessage: "Координати скопійовані в буфер обміну",defaultPattern: "Повернути установки за замовчуванням",goTo: "Перейти до місця розташування",inputCoordTitle: "Вхідні координати",invalidCoordinate: "Неприпустимі координати",liveMode: "Режим реального часу",locationOffBasemap: "Вхідні координати знаходяться за межами екстента базової карти",noBasemap: "Базової карта відсутня.",noLocation: "Немає місця розташування",removeConversion: "Видалити конвертацію",selectFormat: "Вибрати формат",settingsTitle: "Налаштування формата"});
