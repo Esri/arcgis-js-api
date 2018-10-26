84a8457a3c7dbf05dca043bbb0984ba1e7b13d8c
Yevhenii Lukashenko <freewindsua@gmail.com>
2018-10-26 11:07:39 +0300

    Add Ukrainian lang to core

---
+++ core/nls/ua/Units.js
@@ -0,0 +1,25 @@
+// COPYRIGHT © 2018 Esri
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// This material is licensed for use under the Esri Master License
+// Agreement (MLA), and is bound by the terms of that agreement.
+// You may redistribute and use this code without modification,
+// provided you adhere to the terms of the MLA and include this
+// copyright notice.
+//
+// See use restrictions at http://www.esri.com/legal/pdfs/mla_e204_e300/english
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, USA 92373
+// USA
+//
+// email: contracts@esri.com
+//
+// See http://js.arcgis.com/4.9/esri/copyright.txt for details.
+
+define({measures: {length: "Довжина", area: "Площа", volume: "Обсяг", angle: "Кут"},units: {millimeters: {singular: "Міліметр", plural: "міліметри", abbr: "мм"},centimeters: {singular: "сантиметр", plural: "сантиметри", abbr: "см"},decimeters: {singular: "дециметр", plural: "дециметри", abbr: "дм"},meters: {singular: "метр", plural: "метри", abbr: "м"},kilometers: {singular: "кілометр", plural: "кілометри", abbr: "км"},inches: {singular: "дюйм", plural: "дюйми", abbr: "дюйм"},feet: {singular: "фут", plural: "футів", abbr: "фт"},yards: {singular: "ярд", plural: "ярдів", abbr: "ярд"},miles: {singular: "миля", plural: "милі", abbr: "мл"},"nautical-miles": {singular: "морськая миля", plural: "морскі милі", abbr: "ММ"},"us-feet": {singular: "фут (США)", plural: "фути (США)", abbr: "фт"},"square-millimeters": {singular: "квадратний мілліметр", plural: "квадратні міліметри", abbr: "мм²"},"square-centimeters": {singular: "квадратний сантиметр", plural: "квадратні сантиметри", abbr: "см²"},"square-decimeters": {singular: "квадратний дециметр", plural: "квадратні дециметри", abbr: "дм²"},"square-meters": {singular: "квадратний метр", plural: "квадратні метри", abbr: "м²"},"square-kilometers": {singular: "квадратний кілометр", plural: "квадратні кілометри", abbr: "км²"},"square-inches": {singular: "квадратний дюйм", plural: "квадратні дюйми", abbr: "дюйм²"},"square-feet": {singular: "квадратний фут", plural: "квадратні фути", abbr: "фт²"},"square-yards": {singular: "квадратний ярд", plural: "квадратні ярди", abbr: "ярд²"},"square-miles": {singular: "квадратна миля", plural: "квадратні милі", abbr: "мл²"},"square-us-feet": {singular: "квадратний фут (США)", plural: "квадратні фути (США)", abbr: "фт²"},acres: {singular: "акр", plural: "акрів", abbr: "акр"},ares: {singular: "ар", plural: "арів", abbr: "a"},hectares: {singular: "гектар", plural: "гектарів", abbr: "га"},liters: {singular: "літр", plural: "літрів", abbr: "л"},"cubic-millimeters": {singular: "кубічний міліметр", plural: "кубічні міліметри", abbr: "мм³"},"cubic-centimeters": {singular: "кубічний сантиметр", plural: "кубічні сантиметри", abbr: "см³"},"cubic-decimeters": {singular: "кубічний дециметр", plural: "кубічні дециметри", abbr: "дм³"},"cubic-meters": {singular: "кубічний метр", plural: "кубічні метри", abbr: "м³"},"cubic-kilometers": {singular: "кубічний кілометр", plural: "кубічні кілометри", abbr: "км³"},"cubic-inches": {singular: "кубічний дюйм", plural: "кубічні дюйми", abbr: "дюйм³"},"cubic-feet": {singular: "кубічний фут", plural: "кубічні фути", abbr: "фт³"},"cubic-yards": {singular: "кубічний ярд", plural: "кубічні ярди", abbr: "ярд³"},"cubic-miles": {singular: "кубічний миля", plural: "кубічні милі", abbr: "мл³"},radians: {singular: "радіан", plural: "радіани", abbr: ""},degrees: {singular: "градус", plural: "градуси", abbr: "°"}}});
