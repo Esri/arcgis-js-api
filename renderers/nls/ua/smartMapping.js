974a0e8473f24066970af414816efc146a1fa5ef
Yevhenii Lukashenko <freewindsua@gmail.com>
2018-10-26 13:57:51 +0300

    Add Ukrainian lang to Zoom widget, identity, smartMapping

---
+++ renderers/nls/ua/smartMapping.js
@@ -0,0 +1,25 @@
+// COPYRIGHT © 2018 Esri
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// This material is licensed for use under the Esri Master License
+// Agreement (MLA), and is bound by the terms of that agreement.
+// You may redistribute and use this code without modification,
+// provided you adhere to the terms of the MLA and include this
+// copyright notice.
+//
+// See use restrictions at http://www.esri.com/legal/pdfs/mla_e204_e300/english
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, USA 92373
+// USA
+//
+// email: contracts@esri.com
+//
+// See http://js.arcgis.com/4.9/esri/copyright.txt for details.
+
+define({other: "Інше",sumOfCategories: "Сума категорій",predominantCategory: "Переважна категорія",strengthOfPredominance: "Значимість переважання",ageInfo_years: "Вік в роках з ${startTime} до ${endTime}",ageInfo_months: "Возраст в месяцах з ${startTime} до ${endTime}",ageInfo_days: "Вік у місяцях з ${startTime} до ${endTime}",ageInfo_hours: "Вік в годиннику з ${startTime} до ${endTime}",ageInfo_minutes: "Вік в хвилинах з ${startTime} до ${endTime}",ageInfo_seconds: "Вік в секундах з ${startTime} до ${endTime}",relationship: {legendTitle: "Ставлення",HL: "Высокий – Низький",HH: "Высокий – Високий",LL: "Низкий – Низький",LH: "Низкий – Високий",HM: "Высокий – Середній",ML: "Средний – Низький",MM: "Средний – Середній",MH: "Средний – Високий",LM: "Низкий – Середній",HM1: "High - Mild",HM2: "High - Medium",M2L: "Medium - Low",M2M1: "Средний – Помірний",M2M2: "Средний – Середній",M2H: "Medium - High",M1L: "Mild - Low",M1M1: "Помірний – Помірний",M1M2: "Помірний – Середній",M1H: "Mild - High",LM1: "Low - Mild",LM2: "Low - Medium"}});
