38eb2902f881c8288aa7d55f799378f8ccfe2c54
Rene Rubalcava <rrubalcava@esri.com>
2021-04-26 12:04:09 -0700

    4.19. patch

--- support/revision.js
+++ support/revision.js
@@ -2,4 +2,4 @@
 All material copyright ESRI, All Rights Reserved, unless otherwise specified.
 See https://js.arcgis.com/4.19/esri/copyright.txt for details.
 */
-define(["exports"],(function(e){"use strict";const t="20210422",c="d399841cb31e02e4f76b4117a449f428802a5e4f";e.buildDate=t,e.commitHash=c,Object.defineProperty(e,"__esModule",{value:!0})}));
+define(["exports"],(function(e){"use strict";const t="20210423",d="33983ad14c009689ffdf09d4145a0b9e63420892";e.buildDate=t,e.commitHash=d,Object.defineProperty(e,"__esModule",{value:!0})}));
